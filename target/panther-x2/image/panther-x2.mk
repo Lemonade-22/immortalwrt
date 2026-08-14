e4e166309494467d0ffd14a82f16c36f73e8c96d
Lemonade-22 <lx970727@gmail.com>
2026-08-14 15:55:33 +0800

    panther-x2: make device layer idempotent and defer bootloader integration

--- target/panther-x2/image/panther-x2.mk
+++ target/panther-x2/image/panther-x2.mk
@@ -1,13 +1,17 @@
-# Panther X2 device definition.
+# BEGIN Panther X2 integration layer
 #
-# This file is intentionally kept outside target/linux/rockchip. The build
-# preparation script installs it into the active Rockchip image definition.
+# This file is kept outside target/linux/rockchip. The preparation script
+# installs this small definition into the active Rockchip image definition.
+#
+# UBOOT_DEVICE_NAME is intentionally not set yet: VIKINGYFY's U-Boot tree does
+# not currently provide a Panther X2 board. The verified ophub idbloader.img +
+# u-boot.itb flow will be wired in the bootloader/image step separately.
 
-PANTHER_X2_DEVICE_MK='define Device/panther_x2
+define Device/panther_x2
   $(Device/rk3566)
   DEVICE_VENDOR := Panther
   DEVICE_MODEL := X2
   DEVICE_DTS := rk3566-panther-x2
-  UBOOT_DEVICE_NAME := panther-x2-rk3566
 endef
-TARGET_DEVICES += panther_x2'
+TARGET_DEVICES += panther_x2
+# END Panther X2 integration layer
